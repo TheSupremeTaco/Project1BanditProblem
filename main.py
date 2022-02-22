76e4354fff04b19ed1be7399f18b2d739df1ab6b
TheSupremeTaco <maksymlike@gmail.com>
2022-02-22 15:45:29 -0500

    Completed Pt1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+import pandas as pd
 
 class kth_arm_bandit:
     def __init__(self):
@@ -14,18 +15,17 @@ class kth_arm_bandit:
         # Book states mean 0 and variance 1 for Gaussian distribution for action values
         # q_*(a) ~ N(0,1)
         self.actualVal = np.random.normal(loc=0, scale=1, size=self.arms)
-        #self.avgRew = np.zeros((self.maxIter,self.arms),float)
 
     def bandit(self,epslon):
         for i in range(self.maxIter):
             tempProb = random.uniform(0,1)
             if tempProb <= (1 - epslon):
-                if np.argmax(self.Q) == 0:
-                    self.Action = random.choice(self.Q)
+                if self.Q[np.argmax(self.Q)] == 0:
+                    self.A = random.randint(0,9)
                 else:
                     self.A = np.argmax(self.Q)
             else:
-                self.Action = random.choice(self.Q)
+                self.A = random.randint(0,9)
             # R_t ~ N(q_*(a),1)
             R = np.random.normal(loc=self.actualVal[self.A], scale=1)
             self.N[self.A] += 1
@@ -33,27 +33,54 @@ class kth_arm_bandit:
             if self.A == np.argmax(self.actualVal):
                 self.T[i][0] += 1
             self.T[i][1] = self.Q[self.A]
-            #print(self.T)
+
+    def banditData(self,epslon,actualVal,selectData):
+        for i in range(self.maxIter):
+            tempProb = random.uniform(0,1)
+            if tempProb <= (1 - epslon):
+                if self.Q[np.argmax(self.Q)] == 0:
+                    self.A = random.randint(0,9)
+                else:
+                    self.A = np.argmax(self.Q)
+            else:
+                self.A = random.randint(0,9)
+            # R_t ~ N(q_*(a),1)
+            R = np.random.normal(loc=self.actualVal[self.A], scale=1)
+            self.N[self.A] += 1
+            self.Q[self.A] = self.Q[self.A] + ((1/self.N[self.A])*(R-self.Q[self.A]))
+            if self.A == np.argmax(self.actualVal):
+                self.T[i][0] += 1
+            self.T[i][1] = self.Q[self.A]
 
 def bandit_machine(eplson):
-    maxIter = 2000
+    maxIter = 3000
     avgRewardDist = np.zeros([maxIter,2])
 
     for i in range(maxIter):
         test_arm = kth_arm_bandit()
-        test_arm.bandit(0)
+        test_arm.bandit(eplson)
         for j in range(maxIter):
             avgRewardDist[j][0] += test_arm.T[j][0]
             avgRewardDist[j][1] += test_arm.T[j][1]
         print(i)
 
     for i in range(len(avgRewardDist)):
-        avgRewardDist[i][0] /= 2000
-        avgRewardDist[i][1] /= 2000
+        avgRewardDist[i][0] /= maxIter
+        avgRewardDist[i][1] /= maxIter
     return avgRewardDist
 
-bandit_epslon_0 = bandit_machine(0)
-bandit_epslon_0_1 = bandit_machine(0.1)
-bandit_epslon_0_01 = bandit_machine(0.01)
+
+# Part 1
+
+
+# Part 2
+df = pd.read_csv('Ads_Optimisation.csv')
+maxIter = df.shape[0]
+dfCopy = df.transpose()
+test= list(df.columns)
+dfCopy["actualVals"]=df[test].sum(axis=1)
+actualVals = dfCopy.to_numpy()
+
+print(df)
 
 print("Done")
